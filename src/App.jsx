0091fdd5b54d8eed716157018421ce8f1601f2b1
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-11 10:44:16 -0600

    blank vite project for a shopping cart UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <section>
+                <h1 className='text-3xl font-bold underline'>
+                    Hello Shoppers weclome
+                </h1>
+            </section>
+        </>
+    );
+}
+
+export default App;
