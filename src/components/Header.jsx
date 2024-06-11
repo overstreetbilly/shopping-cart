0a89d540aa9611946465590b78a398812258dc15
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-11 13:09:48 -0600

    Added starter code to App, Header and Shop components.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,44 @@
-const Header = () => {
-    return <div>Header</div>;
+import { useRef } from "react";
+import CartModal from "./CartModal.jsx";
+
+const Header = ({ cart, onUpdateCartItemQuantity }) => {
+    const modal = useRef();
+    const cartQuantity = cart.items.length;
+    function handleOpenCartClick() {
+        modal.current.open();
+    }
+    let modalActions = <button>Cloase</button>;
+
+    if (cartQuantity > 0) {
+        modalActions = (
+            <>
+                <button>Close</button>
+                <button>Checkout</button>
+            </>
+        );
+    }
+    return (
+        <>
+            <CartModal
+                ref={modal}
+                cartItems={cart.items}
+                onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+                title='Your Cart'
+                actions={modalActions}
+            />
+            <header id='main-header'>
+                <div id='main-title'>
+                    <img src='logo.png' alt='Elegant model' />
+                    <h1>Elegant Context</h1>
+                    <p>
+                        <button onClick={handleOpenCartClick}>
+                            Cart ({cartQuantity})
+                        </button>
+                    </p>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Header;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,18 @@
-const Shop = () => {
-    return <div>Shop</div>;
+import { DUMMY_PRODUCTS } from "../dummy-products.js";
+import Product from "./Product.jsx";
+const Shop = ({ onAddItemToCart }) => {
+    return (
+        <section id='shop'>
+            <h2>Elegant Clothing For Everyone</h2>
+            <ul id='products'>
+                {DUMMY_PRODUCTS.map((product) => (
+                    <li key={product.id}>
+                        <Product {...product} onAddToCart={onAddItemToCart} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Shop;
